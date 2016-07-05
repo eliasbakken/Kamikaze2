d059b120706ab06ea793cc8f479340bcfc62dcc9
Elias Bakken <elias@iagent.no>
2016-07-05 16:17:18 +0000

    Added Octoprint update and plugins updated

--- make-from-kamikaze1.sh
+++ make-from-kamikaze1.sh
@@ -2,7 +2,7 @@
 
 install_prerequisites() {
 	apt-get update
-	apt-get install -y python-cairo
+	apt-get install -y python-cairo python-gi-cairo
 }
 
 
@@ -31,8 +31,33 @@ install_toggle() {
 	systemctl restart toggle
 }
 
+install_octoprint() {
+	cd /usr/src
+	git clone https://github.com/foosel/OctoPrint
+	cd OctoPrint
+	python setup.py install
+	sed -i.bak s:/usr/bin/octoprint:/usr/local/bin/octoprint:g /lib/systemd/system/octoprint.service
+	systemctl daemon-reload
+	systemctl restart octoprint
+}
+
+install_octoprint_redeem() {
+	cd /usr/src/
+	git clone https://github.com/eliasbakken/octoprint_redeem
+	cd octoprint_redeem
+	python setup.py install
+}
+
+install_octoprint_toggle() {
+	cd /usr/src
+	git clone https://github.com/eliasbakken/octoprint_toggle
+	cd octoprint_toggle
+	python setup.py install
+}
 
 install_prerequisites
-#install_redeem
+install_redeem
 install_toggle
-
+install_octoprint
+install_octoprint_redeem
+install_octoprint_toggle
