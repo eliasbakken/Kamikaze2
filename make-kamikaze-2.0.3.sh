913dfd12449b6a51f6dfaeb7e5568c19357006eb
Elias Bakken <elias@iagent.no>
2016-09-14 16:18:18 +0200

    Added octoprint_toggle and redeem

--- make-kamikaze-2.0.3.sh
+++ make-kamikaze-2.0.3.sh
@@ -6,9 +6,12 @@
 
 # TODO 2.0:
 # Custom uboot
-# sgx-install after changing kernel
+# redeem plugin
+# Toggle plugin
 
 # STAGING: 
+# sgx-install after changing kernel
+#  fatal error: yaml.h: No such file or directory
 # Adafruit lib disregard overlay (Swithed to spidev)
 # consoleblank=0
 
@@ -20,6 +23,7 @@ echo "Making Kamikaze 2.0.3"
 export LC_ALL=C
 
 add_testing_branch() {
+    sed -i 's%#deb-src http://httpredir.debian.org/debian/ jessie main contrib non-free%deb-src http://httpredir.debian.org/debian/ jessie main contrib non-free%' /etc/apt/sources.list
 	cat >/etc/apt/preferences.d/security.pref <<EOL
 Package: *
 Pin: release l=Debian-Security
@@ -36,7 +40,7 @@ Pin: release a=testing
 Pin-Priority: 750
 EOL
 
-    cat >> /etc/apt/sources.list <<EOL
+    cat >/etc/apt/sources.list.d/testing.list <<EOL
 #### testing/Stretch  #########
 deb http://httpredir.debian.org/debian/ testing main contrib non-free
 deb-src http://httpredir.debian.org/debian/ testing main contrib non-free
@@ -58,7 +62,7 @@ stop_services() {
 install_dependencies(){
 	apt-get update --fix-missing
 	apt-get upgrade -y
-	apt-get install -y python-numpy \
+	apt-get install -y \
 	swig \
 	cura-engine \
 	iptables-persistent \
@@ -79,9 +83,12 @@ install_dependencies(){
 	libgudev-1.0-dev \
 	libevdev-dev \
 	libxkbcommon-dev \
-	libmtdev-dev \
-	libudev-dev
+    gir1.2-gtk-3.0 \
+    libgtk-3-0 \
+    libyaml-dev \
+	libmtdev-dev 
 	apt-get install -y -t testing python-scipy
+    apt-get install -t testing python-gi-cairo
 	pip install evdev
 	pip install spidev
 }
@@ -103,10 +110,26 @@ post_redeem() {
 	touch /etc/redeem/local.cfg
 	chown -R octo:octo /etc/redeem/
 
-	# Install systemd script
-	cp systemd/redeem.service /lib/systemd/system
+    cd /usr/src/Kamikaze2
+
+    # Install rules
+    cp scripts/spidev.rules /etc/udev/rules.d/
+
+	# Install Kamikaze2 specific systemd script
+	cp scripts/redeem.service /lib/systemd/system
 	systemctl enable redeem
 	systemctl start redeem
+}
+
+create_user() {
+    default_groups="admin,adm,dialout,i2c,kmem,spi,cdrom,floppy,audio,dip,video,netdev,plugdev,users,systemd-journal,tisdk,weston-launch,xenomai"
+    mkdir /home/octo/
+    mkdir /home/octo/.octoprint
+    useradd -G "${default_groups}" -s /bin/bash -m -p octo -c "OctoPrint" octo
+    chown -R octo:octo /home/octo
+    chown -R octo:octo /usr/local/lib/python2.7/dist-packages
+    chown -R octo:octo /usr/local/bin
+    chmod 755 -R /usr/local/lib/python2.7/dist-packages
 }
 
 install_octoprint() {
@@ -140,6 +163,24 @@ post_octoprint() {
 	systemctl start octoprint
 }
 
+install_octoprint_redeem() {
+	cd /usr/src/
+	if [ ! -d "octoprint_redeem" ]; then
+		git clone https://github.com/eliasbakken/octoprint_redeem
+	fi
+	cd octoprint_redeem
+	python setup.py install
+}
+
+install_octoprint_toggle() {
+	cd /usr/src
+	if [ ! -d "octoprint_toggle" ]; then
+		git clone https://github.com/eliasbakken/octoprint_toggle
+	fi
+	cd octoprint_toggle
+	python setup.py install
+}
+
 install_overlays() {
 	cd /usr/src/
 	git clone https://github.com/eliasbakken/bb.org-overlays
@@ -152,6 +193,9 @@ install_sgx() {
 	tar xfv GFX_5.01.01.02_es8.x.tar.gz -C /
 	cd /opt/gfxinstall/
 	./sgx-install.sh
+    cd /usr/src/Kamkaize2/
+    cp sgx-startup.service /lib/systemd/system/
+    systemctl enable sgx-startup.service
 	depmod -a 4.4.20-bone13
 }
 
@@ -171,7 +215,7 @@ install_clutter() {
 	cd /usr/src
     apt-get build-dep -y -t testing clutter-1.0
     apt-get source -y -t testing clutter-1.0
-	cd clutter-1.26.0
+	cd clutter-1.0-1.26.0
 	./configure --prefix=/usr --libdir=/usr/lib/arm-linux-gnueabihf/ --disable-x11-backend  --enable-egl-backend --enable-evdev-input --disable-gdk-backend
 	make
 	make install
@@ -218,42 +262,39 @@ post_cura() {
     chown octo:octo /home/octo/.octoprint/slicingProfiles/cura/
 }
 
-create_user() {
-    default_groups="admin,adm,dialout,i2c,kmem,spi,cdrom,floppy,audio,dip,video,netdev,plugdev,users,systemd-journal,tisdk,weston-launch,xenomai"
-    mkdir /home/octo/
-    mkdir /home/octo/.octoprint
-    useradd -G "${default_groups}" -s /bin/bash -m -p octo -c "OctoPrint" octo
-    chown -R octo:octo /home/octo
-    chown -R octo:octo /usr/local/lib/python2.7/dist-packages
-    chown -R octo:octo /usr/local/bin
-    chmod 755 -R /usr/local/lib/python2.7/dist-packages
-}
 
 other() {
     sed -i 's/cape_universal=enable/consoleblank=0 fbcon=rotate:1 omap_wdt.nowayout=0/' /boot/uEnv.txt	
 	sed -i 's/beaglebone/kamikaze/' /etc/hostname
 }
 
-add_testing_branch
-stop_services
-install_dependencies
-install_redeem
-post_redeem
-create_user
-install_octoprint
-post_octoprint
-install_overlays
-install_sgx
-install_libinput
-install_glib
-install_cogl
-install_clutter
-install_mx
-install_mash
-install_toggle
-post_toggle
-post_cura
-other
+
+all() {
+    add_testing_branch
+    stop_services
+    install_dependencies
+    install_redeem
+    post_redeem
+    create_user
+    install_octoprint
+    post_octoprint
+    install_octoprint_redeem
+    install_octoprint_toggle
+    install_overlays
+    install_sgx
+    install_libinput
+    install_glib
+    install_cogl
+    install_clutter
+    install_mx
+    install_mash
+    install_toggle
+    post_toggle
+    post_cura
+    other
+}
+
+all
 
 echo "Now reboot!"
 
