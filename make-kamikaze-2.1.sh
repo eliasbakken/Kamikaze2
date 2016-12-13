cbc28ef8f2a0cfbaa0148eee9572680f016e5792
goeland86 <goeland86@gmail.com>
2016-12-14 00:52:27 +0100

    Splitting the script in two. One to prepare the base image of ubuntu into a kamikaze-ready distro, the other to build Kamikaze 2.1.0

--- make-kamikaze-2.1.sh
+++ make-kamikaze-2.1.sh
@@ -46,35 +46,6 @@ echo "**Making ${VERSION}**"
 
 export LC_ALL=C
 
-prep_ubuntu() {
-	echo "** Preparing Ubuntu for kamikaze2 **"
-	cd /opt/scripts/tools/
-	git pull
-	sh update_kernel.sh --bone-kernel --lts-4_1
-	touch /etc/pm/sleep.d/wireless
-	apt-get install unzip iptables
-}
-
-remove_unneeded_packages() {
-	echo "** Remove unneded packages **" 
-
-	rm -rf /etc/apache2/sites-enabled
-	rm -rf /root/.c9
-	rm -rf /usr/local/lib/node_modules
-	rm -rf /var/lib/cloud9
-	rm -rf /usr/lib/node_modules/
-	apt-get purge -y apache2 apache2-bin apache2-data apache2-utils
-}
-
-install_repo() {
-	cat >/etc/apt/sources.list.d/testing.list <<EOL
-#### Kamikaze ####
-deb [arch=armhf] http://kamikaze.thing-printer.com/debian/ stretch main
-EOL
-	wget -q http://kamikaze.thing-printer.com/debian/public.gpg -O- | apt-key add -
-	apt-get update
-}
-
 port_forwarding() {
 	echo "** Port Forwarding **"
 	# Port forwarding
@@ -320,9 +291,9 @@ other() {
 }
 
 dist() {
-	prep_ubuntu
-	remove_unneeded_packages
-	install_repo
+#	prep_ubuntu
+#	remove_unneeded_packages
+#	install_repo
 	reboot
 	port_forwarding
 	install_dependencies
