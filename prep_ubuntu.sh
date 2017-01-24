ddb5917a37934c404bdcc486b788bae5f656d62c
root <root@kamikaze.localdomain>
2017-01-24 21:49:20 +0000

    Commenting out the ubuntu repo created for Kamikaze - it's incomplete or one package is causing grief at the moment.

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -37,10 +37,10 @@ install_repo() {
 	echo "installing Kamikaze repo to the list"
 	cat >/etc/apt/sources.list.d/testing.list <<EOL
 #### Kamikaze ####
-deb [arch=armhf] http://kamikaze.thing-printer.com/ubuntu/ xenial main
+#deb [arch=armhf] http://kamikaze.thing-printer.com/ubuntu/ xenial main
 deb [arch=armhf] http://kamikaze.thing-printer.com/debian/ stretch main
 EOL
-	wget -q http://kamikaze.thing-printer.com/ubuntu/public.gpg -O- | apt-key add -
+#	wget -q http://kamikaze.thing-printer.com/ubuntu/public.gpg -O- | apt-key add -
 	wget -q http://kamikaze.thing-printer.com/debian/public.gpg -O- | apt-key add -
 	apt-get update
 }
